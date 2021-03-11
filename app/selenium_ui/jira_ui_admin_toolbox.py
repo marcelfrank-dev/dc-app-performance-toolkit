39af9519d7aaef3a79ca982f8aa379fc6656c19e
Marcel Frank <M.Frank@Decadis.de>
2021-03-11 16:13:54 +0100

    Add default selenium tests

--- app/selenium_ui/jira_ui_admin_toolbox.py
+++ app/selenium_ui/jira_ui_admin_toolbox.py
@@ -1,13 +1,59 @@
 from extension.jira import extension_ui  # noqa F401
-from selenium_ui.conftest import print_timing
 from selenium_ui.jira import modules
-from selenium_ui.jira.pages.pages import AdminToolboxIssueTypeView, JwtTestIssueView, SumUpCalcRulesView, SumUpCalculationView, SumUpGlobalSettingsView, LastLogView
+from selenium_ui.jira.pages.pages import AdminToolboxIssueTypeView
 
 
 # this action should be the first one
-# ----------------------- ADMIN MODE ------------------------
 def test_0_selenium_a_login(jira_webdriver, jira_datasets, jira_screen_shots):
-    modules.adminLogin(jira_webdriver)
+    modules.login(jira_webdriver, jira_datasets)
+
+
+def test_1_selenium_browse_projects_list(jira_webdriver, jira_datasets, jira_screen_shots):
+    modules.browse_projects_list(jira_webdriver, jira_datasets)
+
+
+def test_1_selenium_browse_boards_list(jira_webdriver, jira_datasets, jira_screen_shots):
+    modules.browse_boards_list(jira_webdriver, jira_datasets)
+
+
+def test_1_selenium_create_issue(jira_webdriver, jira_datasets, jira_screen_shots):
+    modules.create_issue(jira_webdriver, jira_datasets)
+
+
+def test_1_selenium_edit_issue(jira_webdriver, jira_datasets, jira_screen_shots):
+    modules.edit_issue(jira_webdriver, jira_datasets)
+
+
+def test_1_selenium_save_comment(jira_webdriver, jira_datasets, jira_screen_shots):
+    modules.save_comment(jira_webdriver, jira_datasets)
+
+
+def test_1_selenium_search_jql(jira_webdriver, jira_datasets, jira_screen_shots):
+    modules.search_jql(jira_webdriver, jira_datasets)
+
+
+def test_1_selenium_view_backlog_for_scrum_board(jira_webdriver, jira_datasets, jira_screen_shots):
+    modules.view_backlog_for_scrum_board(jira_webdriver, jira_datasets)
+
+
+def test_1_selenium_view_scrum_board(jira_webdriver, jira_datasets, jira_screen_shots):
+    modules.view_scrum_board(jira_webdriver, jira_datasets)
+
+
+def test_1_selenium_view_kanban_board(jira_webdriver, jira_datasets, jira_screen_shots):
+    modules.view_kanban_board(jira_webdriver, jira_datasets)
+
+
+def test_1_selenium_view_dashboard(jira_webdriver, jira_datasets, jira_screen_shots):
+    modules.view_dashboard(jira_webdriver, jira_datasets)
+
+
+def test_1_selenium_view_issue(jira_webdriver, jira_datasets, jira_screen_shots):
+    modules.view_issue(jira_webdriver, jira_datasets)
+
+
+def test_1_selenium_view_project_summary(jira_webdriver, jira_datasets, jira_screen_shots):
+    modules.view_project_summary(jira_webdriver, jira_datasets)
 
 
 """
@@ -15,8 +61,15 @@ Add custom actions anywhere between login and log out action. Move this to a dif
 Write your custom selenium scripts in `app/extension/jira/extension_ui.py`.
 Refer to `app/selenium_ui/jira/modules.py` for examples.
 """
+
+
 # def test_1_selenium_custom_action(jira_webdriver, jira_datasets, jira_screen_shots):
 #     extension_ui.app_specific_action(jira_webdriver, jira_datasets)
+
+
+# ----------------------- ADMIN MODE ------------------------
+def test_1_selenium_a_login(jira_webdriver, jira_datasets, jira_screen_shots):
+    modules.adminLogin(jira_webdriver)
 
 
 # ----------------------- ADMIN TOOLBOX ------------------------
